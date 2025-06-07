2307064806c7734adea3b4e41412da6ec50b0c45
Kijam López <kijamve@gmail.com>
2025-06-07 19:05:55 -0400

    Add customer migration script
    - Create a new file 'migrate-customers.js' for customer data migration
    - Include detailed customer information such as name, ID, address, and email
    - Structure data entries for various subscription plans and payment details
    - Ensure the script is ready for future integration with the migration process

---
+++ migrate-customers.js
@@ -0,0 +1,705 @@
+/*
+#4525 por Jose Gonzalez
+Básica - 1 Sitio
+12,00 U$D / mes
+
+junio 2, 2025	julio 2, 2025
+
+Jose Gonzalez
+V26709194
+Calle 91-2
+Maracaibo
+Capital
+
+Dirección de correo electrónico:
+jjgonzc@gmail.com	
+
+91d7326d-0fc3-4778-8e5a-39f82928aa21
+#####################
+#4512 por Tallal Henaui
+Básica Semestral - 1 Sitio
+60,00 U$D cada 6 meses
+
+mayo 28, 2025	noviembre 28, 2025
+
+TALLAL HENAUI SALAHE
+V11238735
+CARABOBO, VALENCIA, SAN JOSE, Venezuela
++584143462111
+tallalh@hotmail.com
+
+d638952f-8510-4785-b2ec-9a9e306452c4
+#####################
+#4507 por CARLOS DEL CASTILLO
+Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+mayo 28, 2025	diciembre 28, 2025
+
+CARLOS DEL CASTILLO
+Estados Unidos (EEUU)
++17868983865
+rodcarlos11@gmail.com
+
+7497ba78-d1fe-4cd0-938a-6d5e51c888db
+#####################
+#4501 por Gerente Sistemas
+Básica Anual - 1 Sitio
+120,00 U$D / año
+
+mayo 27, 2025	mayo 27, 2026
+
+INVERSIONES APARCITY S A
+J001829610
+AV LA ESTANCIA CC CENTRO COMERCIAL CIUDAD TAMANACO NIVEL LOBBY LOCAL HOTEL CCT URB CHUAO CARACAS (CHACAO) MIRANDA ZONA POSTAL 1061, Venezuela
++582127008000
+gerente.sis@hotelcct.com
+
+ef577a8c-3bc4-4959-bd34-1ee64acee740
+#####################
+#4495 por Liam Fung
+Básica Anual - 1 Sitio
+120,00 U$D / año
+
+mayo 27, 2025	mayo 27, 2026
+
+Liam Fung
+29856925
+mañongo valencia Carabobo, Venezuela
++584125052777
+carovastore@gmail.com
+
+1c61af9e-ed65-4994-b895-02b387e1636e
+#####################
+#4492 por Victor Bitar
+Básica - 1 Sitio
+12,00 U$D / mes
+
+mayo 27, 2025	junio 27, 2025
+
+VICTOR ELIAS BITAR BADDOUR
+V23534481
+MONAGAS, MATURIN, SAN SIMON, Venezuela
++584249480518
+baddourv181@gmail.com
+
+6ddc1985-973b-4258-ad7e-fe622e6222d6
+#####################
+#4490 por Marianna Sapiro
+Básica - 1 Sitio
+10,34 U$D / mes
+
+mayo 26, 2025	junio 26, 2025
+
+Uruguay
+
+plazacafeadm@gmail.com
+
+ad5e24ba-d9f0-4f6d-a505-0c533906d5c3
+#####################
+#4486 por Lucas Sempé
+Básica - 1 Sitio
+10,34 U$D / mes
+
+mayo 26, 2025	junio 26, 2025
+
+Lucas Sempé
+37273624
+Malvinas Rosa quiroga 148, Departamento, Argentina
++543544307585
+sempelucas@gmail.com
+
+d118f72f-7dce-4f92-9b7e-68e9b21cdca7
+#####################
+#4481 por Angelica Rangel
+Básica - 1 Sitio
+12,00 U$D / mes
+
+mayo 23, 2025	junio 23, 2025
+
+ANGELICA MARIA RANGEL MACHADO
+V23692896
+Caracas, Venezuela
++584241913404
+armsalud24@gmail.com
+
+39a09137-8b5d-41b1-9921-829207e7bc26
+#####################
+#4479 por SURTI DONUTS C.A.
+Básica Semestral - 1 Sitio
+60,00 U$D cada 6 meses
+
+mayo 21, 2025	noviembre 21, 2025
+
+SURTI DONUTS C.A.
+J317011503
+Calle Colon, Qta Oracion, PB Nro 19 Loc A, Urb Los Caobos, Caracas, Distrito Capital, Venezuela
++584126095695
+surtidonuts.gest@gmail.com
+	
+21471951-480d-471a-9e22-735a5059863c
+#####################
+#4475 por Leonardo Rafael Grabow
+Básica - 1 Sitio
+10,34 U$D / mes
+
+mayo 20, 2025	junio 20, 2025	
+
+Argentina
+clientesbinweb@gmail.com
+
+76ce6212-d5d9-4ffc-b1a2-4d732e5d70f5
+#####################
+#4461 por Hidrotech Argentina SA
+Básica - 1 Sitio
+10,34 U$D / mes
+
+mayo 15, 2025	junio 15, 2025
+
+jlopez@hidrofil.com.ar
+
+32086a50-3109-4554-938a-1f7adc4a15cc
+#####################
+#4447 por ALEISA Medina
+Básica - 1 Sitio
+12,00 U$D / mes
+
+mayo 9, 2025	En 2 días
+
+Aleisa Medina
+412485563
+Porlamar, Nueva Esparta, Venezuela
++584147911995
+gibranimport@gmqil.com
+
+79bfbabf-e07e-4f63-914e-ca4125b65b1e
+#####################
+#4442 por Pedro Poller
+Membership - Básica - 1 Sitio
+12,00 U$D / mes
+
+mayo 9, 2025	En 2 días
+
+PEDRO JOSE POLLER CASTRO
+V19985896
+GUARICO, ROSCIO, PARAPARA, Venezuela
++584126119034
+aclgestion2024@gmail.com
+
+7adf1496-51d0-40db-abb7-5dc5533381f9
+#####################
+#4437 por Rayner Alvarez
+Básica - 1 Sitio
+12,00 U$D / mes
+
+mayo 9, 2025	2025-06-09
+
+RAYNER RICARDO ALVAREZ GARCIA
+V22410730
+CARABOBO, NAGUANAGUA, NAGUANAGUA, Venezuela
++584145817152
+raynericardo@gmail.com
+
+f3bd2b96-b2d5-4d95-b172-e38bc6d10191
+#####################
+#4425 por Hugo Montilla
+Básica Semestral - 1 Sitio
+60,00 U$D cada 6 meses
+
+mayo 7, 2025	noviembre 7, 2025
+
+HUGO GERARDO MONTILLA LEAL
+V16607915
+ZULIA, MARACAIBO, FRANCISCO EUGENIO, Venezuela
++584146058166
+a1plusve@gmail.com
+
+86d376e1-b180-430e-8de9-eee29f98c9ca
+#####################
+#4400 por Pablo Albarracin
+Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+mayo 2, 2025	noviembre 2, 2025
+
+Pablo Albarracin
+Argentina
+palbarracin@frappe.com.ar
+
+227175d2-7dec-4ec6-97b6-a5a94ede97f0
+#####################
+#4337 por Eduardo Agustin Usandizaga
+Básica Anual - 1 Sitio
+103,45 U$D / año
+
+abril 15, 2025	abril 15, 2026
+
+Eduardo Agustin Usandizaga
+Argentina
+anuessle@sumainversiones.com.ar
+
+1144d333-ca3a-4ac7-a9bb-f3239fe56a9b
+#####################
+#4327 por Julieta Lindner
+Básica - 1 Sitio
+5,17 U$D / mes
+
+abril 10, 2025	2025-06-12
+
+Julieta Lindner
+marittimactivewear@gmail.com
+
+fe0a62c6-f394-4e65-8b3b-14c798d659d2
+#####################
+#4308 por Giomar rogger Rios peralta
+Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+abril 3, 2025	octubre 3, 2025
+giomar rogger rios peralta
+Argentina
+riperink30@gmail.com
+
+54e4fdd6-089a-43b9-bf64-bb985387689e
+#####################
+#4299 por Lucas Bertotti
+Básica Anual - 1 Sitio
+103,45 U$D / año
+
+marzo 31, 2025	marzo 31, 2026
+#####################
+#4296 por VISAGISMO S.R.L.
+Básica - 1 Sitio
+10,34 U$D / mes
+
+marzo 31, 2025	julio 5, 2025
+#####################
+#4288 por Jesus Alvarado
+Básica Anual - 1 Sitio
+120,00 U$D / año
+
+marzo 29, 2025	marzo 29, 2026
+#####################
+#4280 por INVERSIONES CAHERSI
+Básica Anual - 1 Sitio
+120,00 U$D / año
+
+marzo 27, 2025	marzo 27, 2026
+#####################
+#4271 por MATIAS WENDLER CORONEL
+Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+marzo 27, 2025	septiembre 27, 2025
+#####################
+#4266 por Andrés Abreu
+Básica - 1 Sitio
+12,00 U$D / mes
+
+marzo 25, 2025	junio 30, 2025
+#####################
+#4255 por Stockin Lavanda
+Básica - 1 Sitio
+62,07 U$D cada 6 meses
+
+marzo 20, 2025	septiembre 20, 2025
+#####################
+#4239 por Daniel Hernandez
+Básica Anual - 1 Sitio
+120,00 U$D / año
+
+marzo 14, 2025	marzo 14, 2026
+#####################
+#4232 por Miriam Rodriguez
+Pro Anual - 3 Sitios
+320,00 U$D / año
+
+marzo 14, 2025	marzo 14, 2026
+#####################
+#4226 por Javier Chirinos
+Básica - 1 Sitio
+12,00 U$D / mes
+
+marzo 12, 2025	junio 15, 2025
+#####################
+#4165 por Miriam Rodriguez
+Membresía - Básica Anual - 1 Sitio
+120,00 U$D / año
+
+febrero 27, 2025	febrero 27, 2026
+#####################
+#4131 por FRANCO CAIMI
+Membresía - Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+febrero 17, 2025	agosto 17, 2025
+#####################
+#4070 por Sergio Riebra
+Membresía - Básica Semestral - 1 Sitio
+60,00 U$D cada 6 meses
+
+febrero 5, 2025	agosto 5, 2025
+#####################
+#4027 por Martin Gutierrez
+Membresía - Básica - 1 Sitio
+10,34 U$D / mes
+
+enero 24, 2025	junio 28, 2025
+#####################
+#3962 por PPH Sa
+Membresía - Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+enero 3, 2025	julio 3, 2025
+
+#####################
+#3906 por Dalmiro José Yarza Montes
+Membresía - Básica Semestral - 1 Sitio
+60,00 U$D cada 6 meses
+
+diciembre 18, 2024	noviembre 18, 2025
+
+#####################
+#3866 por Angel David
+Membresía - Básica - 1 Sitio
+12,00 U$D / mes
+
+diciembre 9, 2024	En 3 días
+
+#####################
+#3834 por Lucas Bertotti
+Membresía - Básica Anual - 1 Sitio
+103,45 U$D / año
+
+noviembre 29, 2024	noviembre 29, 2025
+Seleccionar Subscription – Nov 27, 2024 @ 02:17 PM	
+#####################
+#3818 por Jairo Feria
+Membership - Básica Anual - 1 Sitio
+120,00 U$D / año
+
+noviembre 27, 2024	noviembre 27, 2025
+
+#####################
+#3804 por Pablo López
+Membresía - Básica Semestral - 1 Sitio
+51,72 U$D cada 6 meses
+
+noviembre 25, 2024	noviembre 25, 2025
+
+#####################
+#3705 por Nicolas Caso
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+octubre 31, 2024	octubre 31, 2025
+
+#####################
+#3668 por MARIO FERNANDEZ CALLE
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+octubre 25, 2024	octubre 25, 2025
+
+#####################
+#3643 por Carlos Bermudez
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+octubre 20, 2024	octubre 20, 2025
+
+#####################
+#3570 por Miguel Ramon Duarte Prieto
+Membresía - Básica - 1 Sitio
+5,17 U$D / mes
+
+octubre 11, 2024	En 20 horas
+
+#####################
+#3565 por Luis Katz
+Membresía - Básica - 1 Sitio
+31,02 U$D cada 6 meses
+
+octubre 11, 2024	En 5 días
+
+#####################
+#3551 por Andry Reyes
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+octubre 9, 2024	diciembre 9, 2025
+
+#####################
+#3536 por Fabrizio Robbio
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+octubre 9, 2024	octubre 9, 2025
+
+#####################
+#3514 por Jonathan Bitriago
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+octubre 2, 2024	octubre 2, 2025
+Seleccionar Subscription – Sep 30, 2024 @ 05:09 PM	
+#####################
+#3499 por Daniel Sanchez
+Membership - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+septiembre 30, 2024	septiembre 30, 2025
+
+#####################
+#3477 por LUCIANO LIVETTI
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+septiembre 24, 2024	septiembre 24, 2025
+
+#####################
+#3461 por Wendy Rojas
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+septiembre 20, 2024	septiembre 20, 2025
+
+#####################
+#3426 por Juan Agustin Traverso
+Membresía - Básica - 1 Sitio
+5,17 U$D / mes
+
+septiembre 10, 2024	junio 15, 2025
+
+#####################
+#3422 por Jonathan Bitriago
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+septiembre 10, 2024	septiembre 10, 2025
+
+#####################
+#3390 por Joel Amén
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+septiembre 2, 2024	septiembre 2, 2025
+
+#####################
+#3380 por Manuel Thielen
+Plan Especial - Ventana Digital
+116,00 U$D cada 5 años
+
+agosto 28, 2024	agosto 28, 2029
+
+#####################
+#3379 por Julio Peñaloza
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 28, 2024	agosto 28, 2025
+
+#####################
+#3370 por Hector Osorio
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 27, 2024	agosto 27, 2025
+
+#####################
+#3332 por Honexis Medina
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 22, 2024	agosto 22, 2025
+Seleccionar Subscription – Aug 21, 2024 @ 05:02 PM	
+#####################
+#3326 por MOISES FRIDZON
+Membership - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 21, 2024	agosto 21, 2025
+Seleccionar Subscription – Aug 21, 2024 @ 02:19 PM	
+#####################
+#3322 por Estación El Consejo S.A
+Membership - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 21, 2024	agosto 21, 2025
+
+#####################
+#3319 por Julio Peñaloza
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+agosto 18, 2024	agosto 18, 2025
+
+#####################
+#3313 por Jhonny Rea
+Membresía - Básica - 1 Sitio
+72,00 U$D / mes
+
+agosto 14, 2024	diciembre 12, 2025
+
+#####################
+#3299 por Carlos Luna
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+agosto 9, 2024	agosto 9, 2025
+
+#####################
+#3288 por Gaston Valerio
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+agosto 6, 2024	agosto 6, 2025
+Seleccionar Subscription – Jul 18, 2024 @ 07:21 PM	
+#####################
+#3207 por Diaz Gabriel Alejandro
+Membership - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 18, 2024	julio 18, 2025
+
+#####################
+#3205 por Carlos Luna
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 18, 2024	julio 18, 2025
+
+#####################
+#3198 por Now de Venezuela
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+julio 17, 2024	julio 17, 2025
+
+#####################
+#3195 por Pablo Toledo
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 16, 2024	julio 16, 2025
+
+#####################
+#3192 por Leonardo Mariño
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 16, 2024	julio 16, 2025
+
+#####################
+#3184 por Jose Primavera
+Membership - Básica - 1 Sitio
+6,00 U$D / mes
+
+julio 15, 2024	junio 16, 2025
+
+#####################
+#3178 por Alaing Maldonado
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+julio 11, 2024	julio 11, 2025
+
+#####################
+#3158 por Nicolas Salazar
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 8, 2024	julio 8, 2025
+
+#####################
+#3133 por Paula MARTINEZ
+Membresía - Básica - 1 Sitio
+5,17 U$D / mes
+
+julio 4, 2024	julio 6, 2025
+
+#####################
+#3112 por Eugenio Cesar Oliden
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+julio 1, 2024	julio 1, 2025
+
+#####################
+#3067 por Hancho Issa
+Membresía - Pro - 3 Sitios
+15,00 U$D / mes
+
+junio 25, 2024	junio 16, 2025
+
+#####################
+#2949 por Keisy Pérez
+Membresía - Básica - 1 Sitio
+6,00 U$D / mes
+
+mayo 28, 2024	En 5 días
+
+#####################
+#2847 por Jesus De la rosa
+Membresía - Básica - 1 Sitio
+5,17 U$D / mes
+
+mayo 10, 2024	junio 15, 2025
+
+#####################
+#2779 por HSIU HUANG
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+mayo 6, 2024	mayo 6, 2026
+
+#####################
+#2735 por Jonathan Bitriago
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+abril 25, 2024	abril 28, 2026
+
+#####################
+#2622 por Facundo Martin Pieroni
+Membresía - Básica - 1 Sitio
+5,17 U$D / mes
+
+abril 2, 2024	agosto 8, 2025
+
+#####################
+#2454 por JESUS ROJAS
+Membresía - Básica Anual - 1 Sitio
+56,90 U$D / año
+
+marzo 7, 2024	marzo 7, 2026
+
+#####################
+#2402 por Isabel Bermudez
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+febrero 28, 2024	marzo 2, 2026
+
+#####################
+#2263 por Liam Fung
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+febrero 5, 2024	febrero 5, 2026
+
+#####################
+#2167 por John David Ferreira Dos Santos
+Membresía - Básica - 1 Sitio
+6,00 U$D / mes
+
+enero 19, 2024	junio 30, 2025
+
+#####################
+#1820 por Karla Patricia Perez Leon
+Membresía - Básica Anual - 1 Sitio
+66,00 U$D / año
+
+noviembre 29, 2023	enero 15, 2026
+
+*/
