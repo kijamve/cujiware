89c8a65e0a1da5f876b48018662bf26cbb217bda
Kijam López <kijamve@gmail.com>
2025-06-08 12:46:31 -0400

    Add server setup and configuration files
    - Update astro.config.mjs to include Node adapter in standalone mode
    - Create server.js to set up an Express server for serving static files and handling Astro routes
    - Add supervisor.conf for process management with Supervisor, including autostart and logging configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 export default defineConfig({
   site: 'https://cujiware.com',
   output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
   integrations: [
     tailwind(),
     sitemap({
